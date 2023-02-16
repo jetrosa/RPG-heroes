7abb13e60db72c85ce2c4c798cdc34dfbef55858
jetrosa <jetro.saarti@gmail.com>
2023-02-16 19:04:57 +0200

    Added abstract Item superclass for armor and weapons.

--- src/main/java/items/Item.java
+++ src/main/java/items/Item.java
@@ -1,2 +1,19 @@
-package items;public class Item {
+package items;
+
+import attributes.HeroAttribute;
+
+public abstract class Item {
+    public enum Slot{ weapon, head, body, legs }
+    private String name;
+    private int requiredLevel;
+    private final Slot SLOT;
+
+    public Slot getSlot() {
+        return SLOT;
+    }
+
+    public Item(Slot slot, int requiredLevel){
+        this.SLOT=slot;
+        this.requiredLevel = requiredLevel;
+    }
 }
